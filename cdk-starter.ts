38bf5f55d6eb68e70a7b56a069b5486557468773
Nyk  Casseano <nykcasseano13@gmail.com>
2023-08-30 23:58:37 -0300

    Add files via upload

---
+++ cdk-starter.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { PhotosStack } from '../lib/PhotosStack';
+import { PhotosHandlerStack } from '../lib/PhotosHandlerStack';
+
+const app = new cdk.App();
+const photosStack = new PhotosStack(app, 'PhotosStack');
+new PhotosHandlerStack(app, 'PhotosHandlerStack',{
+    targetBucketArn: photosStack.photosBucketArn
+})
